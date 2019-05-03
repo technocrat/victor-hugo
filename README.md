a1aa7b3f4e18464f88f24ebb49ebf7c6629f5e24
Richard Careaga <public@careaga.net>
2019-05-03 11:06:39 -0700

    urg

--- README.md
+++ README.md
@@ -4,6 +4,4 @@
 On the internet no one knows you're dog unless you have a website.
 =======
 # victor-hugo
-Locus: Richard Careaga
-A line I wrote on my local computer
-A line I wrote on my local computer
+
--- content/post/pini/R/premap.R
+++
@@ -1,6 +0,0 @@
-no_ylab = ylab("") 
-no_xlab = xlab("")
-
-plain_theme = theme(axis.text=element_blank()) + theme(panel.background = element_blank(), panel.grid = element_blank(), axis.ticks = element_blank())
-
-poly = coord_map("polyconic")
